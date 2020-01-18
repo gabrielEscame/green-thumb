726876fd7a6a4335c6f44ee7f17f2de9c5aea06e
Gabriel Escame <gabriel.escame@gmail.com>
2020-01-18 16:35:52 -0200

    purchase-page-post-rote

--- src/pages/purchase/index.jsx
+++ src/pages/purchase/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './style.css'
 import api from '../../api'
 import Button from '../../components/button'
@@ -15,7 +15,13 @@ import toxicGrey from '../../images/icons/grey/toxic.svg';
 import petGrey from '../../images/icons/grey/pet.svg';
 
 const Purchase = (props) => {
-  const [plant, setPlant] = useState({})
+  const [plant, setPlant] = useState({});
+  const [error, setError] = useState('');
+  const [nameValue, setNameValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [inputClass, setInputClass] = useState('purchase-form-input');
+  const [inputTitleClass, setInputTitleClass] = useState('purchase-form-input-title');
+
 
   useEffect(() => {
     const { id } = props.match.params;
@@ -23,6 +29,48 @@ const Purchase = (props) => {
       .then(response => setPlant(response.data))
       .catch(err => console.log(err))
   }, [props.match.params])
+
+  const onChangeName = useCallback((e) => {
+    let {value} = e.target;
+    const filter = /^[a-zA-Z\s]+$/;
+    if(value === '' || filter.test(value)){
+      setNameValue(e.target.value)
+    }
+  }, [])
+
+  const onChangeEmail = useCallback((e) => {
+    let {value} = e.target;
+    if(value === '' || value) {
+      setEmailValue(e.target.value)
+    }
+  }, [])
+
+  const onClick = () => {
+    const filter = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    ;
+    if(!emailValue || !filter.test(emailValue)){
+      setError('please provide a valid e-mail');
+      setInputClass('input-error');
+      setInputTitleClass('input-title-error')
+    }
+    else {
+      setError('');
+      setInputClass('purchase-form-input');
+      setInputTitleClass('purchase-form-input-title');
+      api.post('front-plantTest-service', {
+        name: nameValue,
+        email: emailValue,
+        id: '1'
+      })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+  
   return (
     <div className='purchase-container'>
       <img className='purchase-logo' src={logo} alt="logo" />
@@ -44,25 +92,26 @@ const Purchase = (props) => {
           <div className='purchase-status-text-plant'>
             <p>{`${plant.sun} sunlight`}</p>
             <p>{`Water ${plant.water}`}</p>
-            <p>{plant.toxicity ? <p><b>Beware!</b> Toxic for pets</p> : <p>Non-toxic for pets</p>}</p>
+            <p>{plant.toxicity ? `Beware! Toxic for pets` : 'Non-toxic for pets'}</p>
           </div>
         </div>
       </div>
       <div className="purchase-form-container">
         <div className='purchase-form-text-container'>
           <h1>Nice Pick!</h1>
-          <p>
+          <p className='purchase-form-p'>
             Tell us your name and e-mail
             and we will get in touch
             regarding your order ;)
           </p>
-          <h2>Name</h2>
-          <input type="text" placeholder='Crazy Plant Person' />
-          <h2>E-mail</h2>
-          <input type="text" placeholder='plantperson@email.com' />
+          <h2 className='purchase-form-input-title'>Name</h2>
+          <input type="text" placeholder='Crazy Plant Person' onChange={onChangeName} value={nameValue} className='purchase-form-input'/>
+          <h2 className={inputTitleClass}>E-mail</h2>
+          <input type="text" placeholder='plantperson@email.com' onChange={onChangeEmail} value={emailValue} className={inputClass}/>
+          <p className='error'>{error}</p>
         </div>
         <div className='purchase-button-container'>
-          <Button label='send' method={() => console.log('enviouuu')} styleName='send-button'/>
+          <Button label='send' method={() => onClick()} styleName='send-button'/>
         </div>
       </div>
     </div>
